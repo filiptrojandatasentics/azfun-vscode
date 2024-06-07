27540eeedcfe0aac58225a8f9dba1df757cf76d8
filip <f.trojan@centrum.cz>
2024-06-07 17:06:41 +0200

    the same from pycharm

--- fastapi_endpoints.py
+++ fastapi_endpoints.py
@@ -5,14 +5,14 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "reporting is up and running"}
+    return {"message": "reporting is up and running as of now"}
 
 
 @app.get("/delinquency/{name}")
 def delinquency_stats(name: str):
     """Return statistics on the delinquency table."""
     try:
-        s = {"name": name}
+        s = {"name": name, "version": "1.0"}
         return s
     except Exception as e:
         raise HTTPException(status_code=404, detail=str(e))
