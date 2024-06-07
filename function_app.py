966d2f4bd68f05b58b9255c8cc22d7985d32cf85
filip <f.trojan@centrum.cz>
2024-06-07 11:06:49 +0200

    a + b = c

--- function_app.py
+++ function_app.py
@@ -2,6 +2,9 @@ import azure.functions as func
 import logging
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+a = 2
+b = 3
+c = a + b
 
 @app.route(route="http_trigger")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
@@ -20,6 +23,6 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             f"This HTTP triggered function executed successfully. a + b = {c}",
              status_code=200
         )
