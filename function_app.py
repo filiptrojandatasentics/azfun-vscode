2883cb464e4fa19d95668579237de49a374504ac
filip <f.trojan@centrum.cz>
2024-06-07 15:51:46 +0200

    today_message

--- function_app.py
+++ function_app.py
@@ -1,16 +1,17 @@
+import datetime
 import azure.functions as func
 import logging
 # from fastapi_endpoints import app as fastapi_app
 
 # app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.FUNCTION)
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
-a = 2
-b = 3
-c = a + b
 
 @app.route(route="http_trigger")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    today = datetime.datetime.now().date()
+    wkday = today.weekday()
+    today_message = f"Today is {wkday}, {today.isoformat()}"
 
     name = req.params.get('name')
     if not name:
@@ -22,9 +23,9 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"Hello, {name}. {today_message}.")
     else:
         return func.HttpResponse(
-             f"This HTTP triggered function executed successfully. a + b = {c}",
+             f"This HTTP triggered function executed successfully. {today_message}",
              status_code=200
         )
