2faa713607f4a204b3196a1a724412af2e6c094b
filip <f.trojan@centrum.cz>
2024-06-11 08:18:28 +0200

    confirm still working

--- fastapi_endpoints.py
+++ fastapi_endpoints.py
@@ -5,14 +5,14 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "reporting is up and running as of Monday 2024-06-10 V2"}
+    return {"message": "reporting is up and running as of Tuesday 2024-06-11"}
 
 
 @app.get("/delinquency/{name}")
 def delinquency_stats(name: str):
     """Return statistics on the delinquency table."""
     try:
-        s = {"name": name, "version": "1.0"}
+        s = {"name": name, "version": "2.0"}
         return s
     except Exception as e:
         raise HTTPException(status_code=404, detail=str(e))
